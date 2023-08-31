bcde4cba8b523ffc46636a56c292d380d2b68b88
Berkay Türkgeldi <berkay.turkgeldi@gmail.com>
2023-08-31 17:37:57 +0300

    Logo above webview is removed

--- app/src/main/java/com/example/webview/MainActivity.kt
+++ app/src/main/java/com/example/webview/MainActivity.kt
@@ -10,16 +10,6 @@ import android.webkit.WebViewClient
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Text
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 
 class MainActivity : ComponentActivity() {
@@ -34,74 +24,57 @@ class MainActivity : ComponentActivity() {
         setContent {
             var webView: WebView? = null
 
-            Column(
-                modifier = Modifier.padding(4.dp),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.spacedBy(8.dp)
-            ) {
-                Image(
-                    painter = painterResource(R.drawable.image_cover),
-                    contentDescription = null,
-                    modifier = Modifier.size(100.dp)
-                )
+            AndroidView(
+                factory = { context ->
+                    WebView(context).apply {
+                        webViewClient = object : WebViewClient() {
 
-                AndroidView(
-                    factory = { context ->
-                        WebView(context).apply {
-                            webViewClient = object : WebViewClient() {
+                            override fun onReceivedError(
+                                view: WebView?,
+                                request: WebResourceRequest?,
+                                error: WebResourceError?
+                            ) {
+                                super.onReceivedError(view, request, error)
+                                view?.loadUrl("about:blank")
 
-                                override fun onReceivedError(
-                                    view: WebView?,
-                                    request: WebResourceRequest?,
-                                    error: WebResourceError?
-                                ) {
-                                    super.onReceivedError(view, request, error)
-                                    view?.loadUrl("about:blank")
-
-                                    val alert = AlertDialog.Builder(context).create()
-                                    alert.setTitle("No internet connection")
-                                    alert.setIcon(android.R.drawable.ic_menu_info_details);
-                                    alert.setCancelable(false);
-                                    alert.setMessage("Please make sure you are connected to the internet and try again")
-                                    alert.setButton(
-                                        AlertDialog.BUTTON_POSITIVE,
-                                        "OK"
-                                    ) { dialog, _ ->
-                                        dialog.dismiss()
-                                    }
-                                    alert.show()
+                                val alert = AlertDialog.Builder(context).create()
+                                alert.setTitle("No internet connection")
+                                alert.setIcon(android.R.drawable.ic_menu_info_details);
+                                alert.setCancelable(false);
+                                alert.setMessage("Please make sure you are connected to the internet and try again")
+                                alert.setButton(
+                                    AlertDialog.BUTTON_POSITIVE,
+                                    "OK"
+                                ) { dialog, _ ->
+                                    dialog.dismiss()
                                 }
-
+                                alert.show()
                             }
-
-                            settings.domStorageEnabled = true
-                            settings.javaScriptEnabled = true
-                            settings.loadWithOverviewMode = false
-                            settings.allowFileAccess = true
-                            settings.javaScriptCanOpenWindowsAutomatically = true
-                            settings.setSupportMultipleWindows(true)
-                            settings.mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
-
-                            //webView?.clearCache(true)
-                            webView = this
-
-                            loadUrl(websiteUrl)
 
                         }
 
-                    }, update = {
-                        webView = it
-                        it.loadUrl(websiteUrl)
-                    }
+                        settings.domStorageEnabled = true
+                        settings.javaScriptEnabled = true
+                        settings.loadWithOverviewMode = false
+                        settings.allowFileAccess = true
+                        settings.javaScriptCanOpenWindowsAutomatically = true
+                        settings.setSupportMultipleWindows(true)
+                        settings.mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
 
-                )
-                BackHandler {
-                    if (webView?.canGoBack() == true) {
-                        webView?.goBack()
+                        webView = this
+                        loadUrl(websiteUrl)
                     }
+                }, update = {
+                    webView = it
+                    it.loadUrl(websiteUrl)
+                }
+
+            )
+            BackHandler {
+                if (webView?.canGoBack() == true) {
+                    webView?.goBack()
                 }
             }
-
         }
     }
 }
